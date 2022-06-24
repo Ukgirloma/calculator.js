64432a1bc4a39903e2f334d8ba79867fa70be695
Ukgirloma <94072290+Ukgirloma@users.noreply.github.com>
2022-06-24 11:13:54 +0100

    calculator with alert

---
+++ index.js
@@ -0,0 +1,25 @@
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
+
+let result;
+
+// using if...else if... else
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+
+// display the result
+alert(`${number1} ${operator} ${number2} = ${result}`);
+
